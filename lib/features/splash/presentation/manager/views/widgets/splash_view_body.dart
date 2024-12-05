a54474dad7aca4ee34d4607980feabcdbde3e5d9
Mustafa ahmed <56519866+mustafa-ahmed-009@users.noreply.github.com>
2024-12-05 14:43:17 +0200

    splash view ui change

--- lib/features/splash/presentation/manager/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/manager/views/widgets/splash_view_body.dart
@@ -1,5 +1,9 @@
+import 'package:bookly/constants.dart';
 import 'package:bookly/core/app_router.dart';
+import 'package:bookly/features/home/presenation/manager/cubit/featured_books_cubit.dart';
+import 'package:bookly/features/home/presenation/manager/cubit/newest_books_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 class SplashViewBody extends StatefulWidget {
@@ -10,26 +14,68 @@ class SplashViewBody extends StatefulWidget {
 }
 
 class _SplashViewBodyState extends State<SplashViewBody> {
+  final TextEditingController _controller = TextEditingController();
+
   @override
   void initState() {
     super.initState();
-
-    navigateToHome();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Image.asset("assets/images/Logo.png"),
-    );
-  }
-
-  void navigateToHome() {
-    Future.delayed(
-      const Duration(milliseconds: 1500),
-      () {
-        GoRouter.of(context).push(AppRouter.kHomeView);
-      },
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(
+          "assets/images/Logo.png",
+          scale: 0.9,
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Container(
+            decoration: BoxDecoration(
+              color: kPrimaryColor,
+              borderRadius: BorderRadius.circular(12),
+              border: Border.all(
+                color: Colors.grey, // Border color
+                width: 1.5, // Border thickness
+              ),
+            ),
+            child: TextField(
+              controller: _controller,
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                hintText: "Please Enter a topic you are intserested in ",
+                border: InputBorder.none,
+              ),
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 12,
+        ),
+        TextButton(
+          onPressed: () {
+            // Check if the input is empty
+            if (_controller.text.trim().isEmpty) {
+              // Show an error or feedback
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text("Please enter a topic before proceeding."),
+                  backgroundColor: kPrimaryColor,
+                ),
+              );
+            } else {
+              GoRouter.of(context).go(AppRouter.kHomeView);
+            }
+          },
+          child: Text("Start"),
+        ),
+      ],
     );
   }
 }
